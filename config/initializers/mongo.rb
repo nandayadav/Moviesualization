363ecdc82694334c8836307956e9942533ac5a9c
Nanda Yadav <nandayadav@gmail.com>
2012-02-02 14:58:03 -0700

    mongohq config

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,5 +1,11 @@
-MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
-MongoMapper.database = "moviesualization-#{Rails.env}"
+# MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
+# MongoMapper.database = "moviesualization-#{Rails.env}"
+
+MongoMapper.config = { 
+  Rails.env => { 'uri' => ENV['MONGOHQ_URL'] || 
+                          'mongodb://localhost/sushi' } }
+
+MongoMapper.connect(Rails.env)
 
 if defined?(PhusionPassenger)
    PhusionPassenger.on_event(:starting_worker_process) do |forked|
