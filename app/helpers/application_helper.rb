0911eb599d1dd88c856be6618fa20d45ec640c4b
Nanda Yadav <nandayadav@gmail.com>
2012-02-02 19:12:45 -0700

    google analytics

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -35,4 +35,22 @@ module ApplicationHelper
         border-color: rgba(0, 0, 0, 0.1) rgba(0, 0, 0, 0.1) rgba(0, 0, 0, 0.25);
     )
   end
+  
+  def google_analytics
+    %(
+      <script type="text/javascript">
+
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-28892550-1']);
+        _gaq.push(['_trackPageview']);
+
+        (function() {
+          var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+          ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+          var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+
+      </script>
+    )
+  end
 end
